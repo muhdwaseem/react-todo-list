ef48153c950ba37ab5cc2156c170d2ac07a6e4be
muhdwaseem <waseemvann@gmail.com>
2021-09-08 18:29:45 +0530

    docs

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,25 @@
-
-import './App.css';
-
+import {useState} from 'react'
+import Counter from './Counter'
 function App() {
-  return (
-    <div className="App">
-      <h1>todo app</h1>
-    </div>
-  );
+  const [count ,setcount] = useState(0)
+
+
+ const addCount =()=>{
+    setcount( count + 1)
+   
+  }
+  let obj ={title:"1st counter",
+  count,
+  place:"man"
+
+  }
+return(
+ <div>
+   <button onClick = {addCount} > ADD</button>
+   <Counter {...obj} />
+   <Counter title="2nd counter" count={count} />
+ </div>
+  )
 }
 
 export default App;
